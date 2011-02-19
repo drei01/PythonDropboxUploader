7b2fc9804cda139d92013794f5f292e1c0fb72d9
drei01 <mcreidie@hotmailc.com>
2011-02-19 13:06:46 +0000

    add the ability to upload files from a directory

--- src/example.py
+++
@@ -1,9 +0,0 @@
-from dbupload import upload_file
-from getpass import getpass
-
-email = raw_input("Enter Dropbox email address:")
-password = getpass("Enter Dropbox password:")
-
-upload_file("example.py","/","dbupload_test.txt",email,password)
-
-print("Uploaded dbupload_test.txt to the root of your Dropbox")
